598f742ec71bb66f44da6b8101644cbd3cd50745
Denishilo <denisanishenko@gmail.com>
2023-05-17 22:21:47 +0300

    fix: changed readme.md

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,10 +2,10 @@ import { AnyAction, applyMiddleware, combineReducers, createStore } from "redux"
 import { appReducer } from "./appReducer";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import thunkMiddleware, { ThunkDispatch } from "redux-thunk";
-import { authReducer } from "../components/loginRegistration/authReducer";
-import { forgotPasswordReducer } from "../components/passwordRecovery/forgotPasswordReducer";
-import { packsReducer } from "../components/packs/packsReducer";
-import { cardsReducer } from "../components/cards/cardsReducer";
+import { authReducer } from "components/loginRegistration/authReducer";
+import { forgotPasswordReducer } from "components/passwordRecovery/forgotPasswordReducer";
+import { packsReducer } from "components/packs/packsReducer";
+import { cardsReducer } from "components/cards/cardsReducer";
 
 export const rootReducer = combineReducers({
   app: appReducer,
@@ -16,19 +16,6 @@ export const rootReducer = combineReducers({
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-
-//export const store = createStore(rootReducer, loadState(), applyMiddleware(thunkMiddleware));
-
-// store.subscribe(() => {
-//   saveState({
-//     app: store.getState().app,
-//     auth: store.getState().auth,
-//     recoveryPassword: store.getState().recoveryPassword,
-//     cards: store.getState().cards,
-//     packs: store.getState().packs,
-//     //settings: store.getState().settings,
-//   });
-// });
 
 export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootReducerType> = useSelector;
